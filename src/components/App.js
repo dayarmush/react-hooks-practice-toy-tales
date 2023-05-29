05a4295992557bea192b833ee0b847ae1205dc9d
dovid yarmush <yarmushda@gmail.com>
2023-05-28 20:04:47 -0400

    render toy cards

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,7 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([])
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
@@ -18,7 +19,7 @@ function App() {
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+      <ToyContainer toys={toys} setToys={setToys} />
     </>
   );
 }
--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ToyCard from "./ToyCard";
 
-function ToyContainer() {
+function ToyContainer({ toys, setToys}) {
+
+  useEffect(() => {
+    fetch('http://localhost:3001/toys')
+    .then(resp => resp.json())
+    .then(toysArr => setToys(toysArr))
+  }, [])
+
+  const renderToys = toys.map(toy => {
+    return <ToyCard key={toy.id} toy={toy} />
+  })
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{renderToys}</div>
   );
 }
 
