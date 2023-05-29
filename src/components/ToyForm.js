22bcd6558b7dc5faf022a2fc13a10fa0931aec38
dovid yarmush <yarmushda@gmail.com>
2023-05-28 20:37:06 -0400

    controlled toy form with submit post

--- src/components/ToyForm.js
+++ src/components/ToyForm.js
@@ -1,15 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 
-function ToyForm() {
+function ToyForm({ setToys }) {
+
+  const blankToyForm = {
+    name: '',
+    image: '',
+    likes: 0
+  }
+
+  const [toyForm, setToyForm] = useState(blankToyForm)
+
+  const handleChange = (e) => {
+    const key = e.target.name
+    const value = e.target.value
+
+    setToyForm({...toyForm, [key]: value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    fetch('http://localhost:3001/toys', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(toyForm)
+    })
+    .then(resp => resp.json())
+    .then(data => {
+      setToys(preToys => [...preToys, data])
+      setToyForm(blankToyForm)
+    })
+  }
+
   return (
     <div className="container">
-      <form className="add-toy-form">
+      <form className="add-toy-form" onSubmit={handleSubmit} >
         <h3>Create a toy!</h3>
         <input
           type="text"
           name="name"
           placeholder="Enter a toy's name..."
           className="input-text"
+          onChange={handleChange}
         />
         <br />
         <input
@@ -17,6 +51,7 @@ function ToyForm() {
           name="image"
           placeholder="Enter a toy's image URL..."
           className="input-text"
+          onChange={handleChange}
         />
         <br />
         <input
